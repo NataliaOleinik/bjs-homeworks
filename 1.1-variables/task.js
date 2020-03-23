4e33d903f5fb78e0daa34305e83e7983a04f0e67
Natalia Oleinik <olnataly81@gmail.com>
2020-03-23 12:33:03 +0700

    task completed

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,27 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
+	let algebra;
+	let geography;
+	let phisics;
     
-	return // ваш ответ;
+	return (a + g + p) / 3;
 }
 
 function sayHello(userName) {
 	
-    // реализуйте здесь задание №2
+	let myName = userName;
+	let message = `Привет, Мир! Меня зовут ${myName}`;
     
-	return // ваш ответ;
+	return message;
 }
 
 function calculateFormula() {
     
-	// реализуйте здесь задание №3
+	let x = 2;
+	let y = 22;
+	let z = 0;
+	let result = x * y + 5 * z + x - 1;
     
-    return // ваш ответ;
+    return result;
 }
