3e36fe72ab72ae4ffd9782828fa11f6c7ad99ce6
Natalia Oleinik <olnataly81@gmail.com>
2020-03-30 14:34:28 +0700

    changes added

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -2,10 +2,8 @@
 function getResult(a,b,c){
     let discriminant = b**2 - 4 * a * c;
     let x = [];
-    if(discriminant < 0){
-        return x;
-    }
-    else if(discriminant == 0){
+
+    if(discriminant == 0){
         x[0] = -b/(2*a);
     }
     else if(discriminant > 0){
@@ -18,22 +16,17 @@ function getResult(a,b,c){
 function getAverageMark(marks){
     let sum = 0;
     let averageMark = 0;
+
     if(marks.length == 0){
         return 0;
     }
     else if(marks.length > 5){
         console.log('Колличество данных превышает установленное значение');
         marks.splice(5);
+    }
     for(let i = 0; i < marks.length; i++){
         sum += marks[i];  
         averageMark = sum/marks.length;
-        } 
-    } 
-    else{
-    for(let i = 0; i < marks.length; i++){
-        sum += marks[i];  
-        averageMark = sum/marks.length;
-        } 
     }
     return averageMark;
 }
@@ -42,10 +35,11 @@ function askDrink(name,dateOfBirthday){
     let year = new Date().getFullYear();
     let age = year - dateOfBirthday.getFullYear();
     let result;
+
     if(age >= 18){
        result = `Не желаете ли олд-фэшн, ${name}?`;
     }
-    else if(age < 18){
+    else{
        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
     }
     return result;
--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,23 @@
+"use strict"
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    if(typeof(percent) == "string" || typeof(contribution) == "string" || typeof(amount) == "string" || typeof(date) == "string"){
+        percent = Number.parseInt(percent);
+        contribution = Number.parseInt(contribution);
+        amount = Number.parseInt(amount);
+        date = Number.parseInt(date);
+    }
+    let S = amount - contribution;
+    let P = (percent/100/12);
+    date = (date.getFullYear() - new Date().getFullYear()) * 12 + ((date.getMonth() + 1)  - (new Date().getMonth() + 1));
+    let monthPayment = S * (P + P / (((1 + P)**date) - 1));
+    let totalAmount = parseFloat((monthPayment * date).toFixed(2));
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+   if(typeof(name) !== "string"){
+       name = "Аноним";
+   } 
+   let greeting = `Привет, мир! Меня зовут ${name}.`
+   return greeting;
 }
